a9413a035a1b64c03cbe8e7071f290e18d266aec
sukanya-ggez <sukanya.u@kkumail.com>
2026-07-08 03:00:23 +0000

    Update: Basic_command.sh

---
+++ basic_command.sh
@@ -0,0 +1,8 @@
+# add all changes to the staging area
+git add .
+# commit the changes with a message
+git commit -m "Your commit message here"
+# push the changes to the remote repository on the specified branch
+git push origin <remote-branch-name>
+#switch to a different branch
+git switch <branch-name>
